6a2a6dcef102b1acd916101259181f2b6d892f3a
mattiabenelli <benellimattia0@gmail.com>
2023-05-08 17:24:15 +0200

    es prodotti done

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,12 @@
+package org.lessons.java.shop;
+
+public class Main {
+	
+	public static void main(String[] args) {
+			
+		Prodotto p1 = new Prodotto("prodotto1", "descrizione", 100, 22);
+		
+		System.out.println(p1);
+
+	}
+}
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 	
 	private int codice;
@@ -8,29 +10,82 @@ public class Prodotto {
 	private int prezzo;
 	private int iva;
 	
-	public Prodotto(int codice, String nome, String descrizione, int prezzo,  int iva) {
+	Random r = new Random();
+	
+	public Prodotto(String nome, String descrizione, int prezzo,  int iva) {
 		
-		setCodice(codice);
+		codice = r.nextInt(999999999);
 		setNome(nome);
 		setDescrizione(descrizione);
 		setPrezzo(prezzo);
 		setIva(iva);
+
+	}
+	
+//	-------------------------------
+	
+	public String getFullName() {
+		return codice + "-" + nome;
 	}
 	
 	public int getCodice() {
 		return codice;
 	}
 	
-	public void setCodice(int codice) {
-		this.codice = codice;
-	}
-	
-	
-	public int getNome() {
+//	-------------------------------
+	public String getNome() {
 		return nome;
 	}
 	
-	public void setNome(int nome) {
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+//	-------------------------------
+	public String getDescrizione() {
+		return descrizione;
+	}
+	
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+//	-------------------------------
+	public int getPrezzo() {
+		return prezzo;
+	}
+	
+	public void setPrezzo(int prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+//	-------------------------------
+	
+	public int getIvaPrice() {
+		
+		return (int) (getPrezzo() * (1 + getIva() / 100f));
+	}
+	
+//	-------------------------------
+	
+	public int getIva() {
+		return iva;
+	}
+	
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+	
+//	-------------------------------
+	
+	@Override
+	public String toString() {
+
+		return "[" + getCodice() + "] " + getNome()
+			+ " - \"" + getFullName() + "\""
+			+ "\n" + getDescrizione()
+			+ "\n" + getPrezzo()  + "$ (" + getIva() + "%)"
+			+ " --> " + getIvaPrice() + "$";
+	}
+	
 }
