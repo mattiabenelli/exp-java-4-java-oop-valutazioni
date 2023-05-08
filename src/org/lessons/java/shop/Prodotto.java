d98c675568ae44eb5a6af404232c96ffd32b5b3d
mattiabenelli <benellimattia0@gmail.com>
2023-05-08 15:36:09 +0200

    setters and getters done

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,36 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-
+	
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private int prezzo;
+	private int iva;
+	
+	public Prodotto(int codice, String nome, String descrizione, int prezzo,  int iva) {
+		
+		setCodice(codice);
+		setNome(nome);
+		setDescrizione(descrizione);
+		setPrezzo(prezzo);
+		setIva(iva);
+	}
+	
+	public int getCodice() {
+		return codice;
+	}
+	
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+	
+	
+	public int getNome() {
+		return nome;
+	}
+	
+	public void setNome(int nome) {
+		this.nome = nome;
+	}
 }
